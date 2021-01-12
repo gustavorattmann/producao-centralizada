00bdd7afb906d301d3fbaf187216177bb4f7a2e7
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-01-12 02:01:29 -0300

    Configurações do sistema, como diretórios para controllers e models e configurações de banco de dados e redis

---
+++ config/config.php
@@ -0,0 +1,30 @@
+<?php
+
+    use Phalcon\Config;
+
+    $config = new Config(
+        [
+            'database' => [
+                'adapter'  => 'Mysql',
+                'host'     => $_ENV['DB_HOST'],
+                'port'     => $_ENV['DB_PORT'],
+                'username' => $_ENV['DB_USERNAME'],
+                'password' => $_ENV['DB_PASSWORD'],
+                'dbname'   => $_ENV['DB_DATABASE']
+            ],
+            'redis' => [
+                'host' => $_ENV['REDIS_HOST'],
+                'port' => $_ENV['REDIS_PORT'],
+                'auth' => $_ENV['REDIS_PASSWORD']
+            ],
+            'application' => [
+                'controllersDir' => CONTROLLERS_PATH,
+                'modelsDir'      => MODELS_PATH,
+                'baseUri'        => '/'
+            ]
+        ]
+    );
+
+    return $config;
+
+?>
