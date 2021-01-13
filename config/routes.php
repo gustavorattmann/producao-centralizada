d02a8274d2192ef26482d50a878c9d74e94c4ab3
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-01-13 02:14:09 -0300

    Adicionado novas rotas

--- config/routes.php
+++ config/routes.php
@@ -4,10 +4,25 @@
     use App\Controllers\UserController;
 
     $users = new MicroCollection();
-    $users->setHandler(new UserController());
-    $users->setPrefix('/api/users');
-    $users->get('/', 'index');
+    $users->setHandler(new UserController())
+          ->setPrefix('/api/users')
+          ->get('/', 'index')
+        //->get('/', 'index')
+          ->post('/register', 'register')
+          ->post('/login', 'login')
+          ->get('/logout', 'logout');
 
     $app->mount($users);
+
+    $app->notFound(
+        function () use ($app) {
+            $app
+                ->response
+                ->setStatusCode(404)
+                ->sendHeaders()
+                ->setContent('Página não encontrada...')
+                ->send();
+        }
+    );
 
 ?>
