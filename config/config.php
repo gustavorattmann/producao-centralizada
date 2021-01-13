a652aafd34281f75addae8881197f11582643af8
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-01-13 02:14:49 -0300

    Adicionado charset como UTF-8

--- config/config.php
+++ config/config.php
@@ -10,7 +10,10 @@
                 'port'     => $_ENV['DB_PORT'],
                 'username' => $_ENV['DB_USERNAME'],
                 'password' => $_ENV['DB_PASSWORD'],
-                'dbname'   => $_ENV['DB_DATABASE']
+                'dbname'   => $_ENV['DB_DATABASE'],
+                'options'  => [
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
+                ]
             ],
             'redis' => [
                 'host' => $_ENV['REDIS_HOST'],
