cc56480149e433072ceef4878ecdb5299a90273e
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-04 01:45:06 -0300

    Criado função de atualização e remoção de usuário, também corrigido algumas funções para usar a instância usuário do model

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -13,8 +13,6 @@
     {
         public function index()
         {
-            $user = new User();
-
             $request = new Request();
 
             $response = new Response();
@@ -139,7 +137,7 @@
                 if ( empty($request->get('name')) && empty($request->get('email')) && empty($request->get('password'))
                   && empty($request->get('level')) && empty($request->get('situation')) ) {
                     $contents = [
-                        'msg' => 'Dados insuficiente!'
+                        'msg' => 'Dados incompletos!'
                     ];
     
                     $response
@@ -232,6 +230,251 @@
             }
         }
 
+        public function update($id = NULL)
+        {
+            $user = new User();
+
+            $request = new Request();
+
+            $response = new Response();
+
+            $security = new Security();
+
+            session_start();
+
+            $bearerToken = $request->getHeaders()['Authorization'];
+
+            $bearerToken = str_replace('Bearer ', '', $bearerToken);
+
+            if ( !empty($_SESSION['user']) && !empty($bearerToken) ) {
+                $key = base64_encode($_ENV['SECRET_KEY'] . $_SESSION['user']);
+
+                JWT::$leeway = 60;
+                $token = JWT::decode($bearerToken, $key, array('HS512'));
+            }
+
+            $token_array = (array) $token;
+            $nbf_array = (array) $token_array['nbf'];
+
+            if ( date(\DateTime::ISO8601) <= $nbf_array && $token_array['situation'] == 1 ) {
+                if ( empty($id) ) {
+                    $id = $token_array['id'];
+                }
+
+                $sql_verify = '
+                    SELECT
+                        *
+                    FROM
+                        users
+                    WHERE
+                        id = :id
+                ';
+
+                $query = $this->db->query(
+                    $sql_verify,
+                    [
+                        'id' => $id
+                    ]
+                );
+
+                $result = $query->fetch();
+
+                if ( ( !empty($request->get('name')) || !empty($request->get('email')) || !empty($request->get('level')) || !empty($request->get('situation')) ) &&
+                     ( ($request->get('name') != $result['name']) || ($request->get('email') != $result['email']) ||
+                     ($request->get('level') != $result['level']) || ($request->get('situation') != $result['situation']) ) ) {
+                    if ( $request->get('name') != $result['name'] ) {
+                        $user->setName($request->get('name'));
+                    } else {
+                        $user->setName($result['name']);
+                    }
+
+                    if ( $request->get('email') != $result['email'] ) {
+                        $user->setEmail($request->get('email'));
+                    } else {
+                        $user->setEmail($result['email']);
+                    }
+
+                    if ( $request->get('situation') != $result['situation'] ) {
+                        $user->setSituation($request->get('situation'));
+                    } else {
+                        $user->setSituation($result['situation']);
+                    }
+
+                    if ( $token_array['level'] == 0 ) {
+                        if ( $request->get('level') != $result['level'] ) {
+                            $user->setLevel($request->get('level'));
+                        } else {
+                            $user->setLevel($result['level']);
+                        }
+                    } else {
+                        $user->setLevel(2);
+                    }
+
+                    $sql = '
+                        UPDATE
+                            users
+                        SET
+                            name      =: name,
+                            email     =: email,
+                            level     =: level,
+                            situation =: situation
+                        WHERE
+                            id        = :id
+                    ';
+
+                    try {
+                        $this->db->begin();
+
+                        $update = $this->db->execute(
+                            $sql,
+                            [
+                                'name'      => $user->getName(),
+                                'email'     => $user->getEmail(),
+                                'level'     => $user->getLevel(),
+                                'situation' => $user->getSituation(),
+                                'id'        => $id
+                            ]
+                        );
+
+                        if ( $update ) {
+                            $contents = [
+                                'msg' => 'Usuário atualizado com sucesso!'
+                            ];
+    
+                            $response
+                                ->setJsonContent($contents, JSON_PRETTY_PRINT, 200)
+                                ->send();
+                        } else {
+                            $contents = [
+                                'msg' => 'Não foi possível atualizar usuário!'
+                            ];
+    
+                            $response
+                                ->setJsonContent($contents, JSON_PRETTY_PRINT, 400)
+                                ->send();
+                        }
+
+                        $this->db->commit();
+                    } catch (Exception $error) {
+                        $this->db->rollback();
+
+                        $contents = [
+                            'msg' => 'Ocorreu um erro em nosso servidor, tente mais tarde!'
+                        ];
+        
+                        $response
+                            ->setJsonContent($contents, JSON_PRETTY_PRINT, 500)
+                            ->send();
+                    }
+                } else {
+                    $contents = [
+                        'msg' => 'Preencha pelo menos um campo com valor diferente do atual!'
+                    ];
+    
+                    $response
+                        ->setJsonContent($contents, JSON_PRETTY_PRINT, 401)
+                        ->send();
+                }
+            } else {
+                $contents = [
+                    'msg' => 'Token inválido!'
+                ];
+
+                $response
+                    ->setJsonContent($contents, JSON_PRETTY_PRINT, 401)
+                    ->send();
+            }
+        }
+
+        public function changePassword($id = NULL) {
+            
+        }
+
+        public function delete($id = NULL)
+        {
+            $request = new Request();
+
+            $response = new Response();
+
+            session_start();
+
+            $bearerToken = $request->getHeaders()['Authorization'];
+
+            $bearerToken = str_replace('Bearer ', '', $bearerToken);
+
+            if ( !empty($_SESSION['user']) && !empty($bearerToken) ) {
+                $key = base64_encode($_ENV['SECRET_KEY'] . $_SESSION['user']);
+
+                JWT::$leeway = 60;
+                $token = JWT::decode($bearerToken, $key, array('HS512'));
+            }
+
+            $token_array = (array) $token;
+            $nbf_array = (array) $token_array['nbf'];
+
+            if ( date(\DateTime::ISO8601) <= $nbf_array && $token_array['situation'] == 1 ) {
+                if ( empty($id) ) {
+                    $id = $token_array['id'];
+                }
+
+                $sql = '
+                    DELETE
+                        users
+                    WHERE
+                        id = :id
+                ';
+
+                try {
+                    $this->db->begin();
+
+                    $del = $this->db->execute(
+                        $sql,
+                        [
+                            'id' => $id
+                        ]
+                    );
+
+                    if ( $del ) {
+                        $contents = [
+                            'msg' => 'Usuário deletado com sucesso!'
+                        ];
+
+                        $response
+                            ->setJsonContent($contents, JSON_PRETTY_PRINT, 200)
+                            ->send();
+                    } else {
+                        $contents = [
+                            'msg' => 'Não foi possível deletar usuário!'
+                        ];
+
+                        $response
+                            ->setJsonContent($contents, JSON_PRETTY_PRINT, 400)
+                            ->send();
+                    }
+
+                    $this->db->commit();
+                } catch(Exception $error) {
+                    $this->db->rollback();
+
+                    $contents = [
+                        'msg' => 'Ocorreu um erro em nosso servidor, tente mais tarde!'
+                    ];
+    
+                    $response
+                        ->setJsonContent($contents, JSON_PRETTY_PRINT, 500)
+                        ->send();
+                }
+            } else {
+                $contents = [
+                    'msg' => 'Token inválido!'
+                ];
+
+                $response
+                    ->setJsonContent($contents, JSON_PRETTY_PRINT, 401)
+                    ->send();
+            }
+        }
+
         public function login()
         {
             $user = new User();
@@ -285,6 +528,8 @@
                     );
     
                     if ( $email_verified ) {
+                        $user->setEmail($request->get('email'));
+
                         $sql_verify_password = '
                             SELECT
                                 password
@@ -297,13 +542,15 @@
                         $password_verify = $this->db->query(
                             $sql_verify_password,
                             [
-                                'email' => $request->get('email')
+                                'email' => $user->getEmail()
                             ]
                         );
     
                         $result = $password_verify->fetch();
+
+                        $user->setPassword($result['password']);
     
-                        if ( $security->checkHash($request->get('password'), $result['password']) ) {
+                        if ( $security->checkHash($request->get('password'), $user->getPassword()) ) {
                             $sql_verify_situation = '
                                 SELECT
                                     id, level, situation
@@ -316,38 +563,42 @@
                             $verify_situation = $this->db->query(
                                 $sql_verify_situation,
                                 [
-                                    'email' => $request->get('email')
+                                    'email' => $user->getEmail()
                                 ]
                             );
     
                             $result = $verify_situation->fetch();
+
+                            $user->setId($result['id']);
+                            $user->setLevel($result['level']);
+                            $user->setSituation($result['situation']);
     
-                            if ( $result['situation'] == 1 ) {
-                                $key = base64_encode($_ENV['SECRET_KEY'] . $request->get('email'));
+                            if ( $user->getSituation() == 1 ) {
+                                $key = base64_encode($_ENV['SECRET_KEY'] . $user->getEmail());
 
                                 $payload = array(
                                     "iss"       => $_SERVER['HOST_NAME'],
                                     "aud"       => $_ENV['APP_URL'],
                                     "iat"       => date(\DateTime::ISO8601),
                                     "nbf"       => date(\DateTime::ISO8601, strtotime('+3 minute')),
-                                    'id'        => $result['id'],
-                                    'level'     => $result['level'],
-                                    'situation' => $result['situation']
+                                    'id'        => $user->getId(),
+                                    'level'     => $user->getLevel(),
+                                    'situation' => $user->getSituation()
                                 );
                                 
                                 try {
                                     $token = JWT::encode($payload, $key, 'HS512');
     
-                                    if ( $this->redis->exists($request->get('email')) ) {
-                                        $this->redis->del($request->get('email'));
+                                    if ( $this->redis->exists($user->getEmail()) ) {
+                                        $this->redis->del($user->getEmail());
                                     }
 
                                     if ( empty($_SESSION['user']) ) {
                                         unset($_SESSION['user']);
                                     }
         
-                                    if ( $this->redis->set($request->get('email'), $token) ) {
-                                        $_SESSION['user'] = $request->get('email');
+                                    if ( $this->redis->set($user->getEmail(), $token) ) {
+                                        $_SESSION['user'] = $user->getEmail();
 
                                         $contents = [
                                             'token' => $token
