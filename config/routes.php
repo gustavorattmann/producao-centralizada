7325b94c4c36a7d2d9d4d3b57e5c73e55701bbee
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-06 18:56:10 -0300

    Criado rotas para os produtos

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,7 @@
 
     use Phalcon\Mvc\Micro\Collection as MicroCollection;
     use App\Controllers\UserController;
+    use App\Controllers\ProductController;
 
     $users = new MicroCollection();
     $users->setHandler(new UserController())
@@ -18,6 +19,16 @@
           ->get('/logout', 'logout');
 
     $app->mount($users);
+
+    $products = new MicroCollection();
+    $products->setHandler(new ProductController())
+             ->setPrefix('/api/products')
+             ->get('/', 'index')
+             ->post('/register', 'register')
+             ->put('/update/{id}', 'update')
+             ->delete('/delete/{id}', 'delete');
+
+    $app->mount($products);
 
     $app->notFound(
         function () use ($app) {
