6ccdf79677ef8fb9f557a058a689053087ca32c5
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-07 23:22:21 -0300

    Renomeado a chave para singular ao invés de plural

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -53,7 +53,7 @@
                                 if ( !empty($products) ) {
                                     foreach ($products as $key => $product) {
                                         $contents[$key] = [
-                                            'products' => [
+                                            'product' => [
                                                 'id'       => $product['id'],
                                                 'name'     => $product['name'],
                                                 'category' => $product['category']
