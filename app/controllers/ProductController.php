3045868ea1f99445cbabf8968326a13210386f6a
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-08 22:37:41 -0300

    Removido instância do modelo de dados da função index

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -12,8 +12,6 @@
     {
         public function index()
         {
-            $product = new Product();
-            
             $request = new Request();
 
             $response = new Response();
