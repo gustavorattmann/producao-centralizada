8da2159ea6928c67ddbfc2a1cff64f69f44439dd
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-09 01:17:20 -0300

    Criado a classe responsável pelo gerenciamento de pedidos

---
+++ app/controllers/OrdersController.php
@@ -0,0 +1,34 @@
+<?php
+
+    namespace App\Controllers;
+
+    use Phalcon\Mvc\Controller;
+    use Phalcon\Http\Request;
+    use Phalcon\Http\Response;
+    use Firebase\JWT\JWT;
+    use App\Models\Orders;
+
+    class OrdersController extends Controller
+    {
+        public function index()
+        {
+            
+        }
+
+        public function register()
+        {
+
+        }
+
+        public function update($id)
+        {
+
+        }
+
+        public function delete($id)
+        {
+            
+        }
+    }
+
+?>
