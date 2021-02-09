d196186725db34dd1aaae7b5c970344587f3b128
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-09 01:51:12 -0300

    Criado classe de produção

---
+++ app/controllers/ProductionController.php
@@ -0,0 +1,34 @@
+<?php
+
+    namespace App\Controllers;
+
+    use Phalcon\Mvc\Controller;
+    use Phalcon\Http\Request;
+    use Phalcon\Http\Response;
+    use Firebase\JWT\JWT;
+    use App\Models\Production;
+
+    class ProductionController extends Controller
+    {
+        public function index()
+        {
+            
+        }
+
+        public function product($id)
+        {
+
+        }
+
+        public function produced()
+        {
+
+        }
+
+        public function report()
+        {
+
+        }
+    }
+
+?>
