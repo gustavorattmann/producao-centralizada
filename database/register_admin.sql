9a8af61a474435ef3fed913268df9308c33c3670
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-09 00:31:05 -0300

    Cadastro do usuário administrador

---
+++ database/register_admin.sql
@@ -0,0 +1,6 @@
+USE producao_centralizada;
+
+INSERT INTO users
+	(name, email, password, level, situation)
+VALUES
+	('Administrator', 'admin@producaocentralizada.com.br', '$2y$10$UFo1Uk9sZSt6bTFwN1I1Ueh3NkIBSdMw53aczNsDUkcNC88CMxs3a', 0, 1);
