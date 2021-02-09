9e055285ea39a9a91d9df3a4f105b53dd6a39528
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-08 22:55:47 -0300

    Funções de alteração e remoção de matéria-prima concluídas

--- app/controllers/RawMaterialController.php
+++ app/controllers/RawMaterialController.php
@@ -279,12 +279,341 @@
 
         public function update($id)
         {
+            $raw_material = new RawMaterial();
+            
+            $request = new Request();
 
+            $response = new Response();
+            
+            session_start();
+
+            if ( !empty($_SESSION['user']) && $this->redis->exists($_SESSION['user']) ) {
+                $bearerToken = $request->getHeaders()['Authorization'];
+
+                $bearerToken = str_replace('Bearer ', '', $bearerToken);
+
+                if ( !empty($bearerToken) && $bearerToken == $this->redis->get($_SESSION['user']) ) {
+                    $key = base64_encode($_ENV['SECRET_KEY'] . $_SESSION['user']);
+
+                    JWT::$leeway = 60;
+                    $token = JWT::decode($bearerToken, $key, array('HS512'));
+
+                    $token_array = (array) $token;
+                    $nbf_array = (array) $token_array['nbf'];
+
+                    if ( date(\DateTime::ISO8601) <= $nbf_array ) {
+                        if ( intval($token_array['situation']) == 1 ) {
+                            if ( intval($token_array['level']) == 0 || intval($token_array['level']) == 1 ) {
+                                if ( !empty($request->getPut('name')) && !empty($request->getPut('stock')) ) {
+                                    $raw_material->setId($id);
+
+                                    $sql_verify_raw_material = '
+                                        SELECT
+                                            *
+                                        FROM
+                                            raw_materials
+                                        WHERE
+                                            id = :id
+                                    ';
+
+                                    $query_verify_raw_material = $this->db->query(
+                                        $sql_verify_raw_material,
+                                        [
+                                            'id' => $raw_material->getId()
+                                        ]
+                                    );
+
+                                    $row = $query_verify_raw_material->numRows();
+                                    $result = $query_verify_raw_material->fetch();
+
+                                    if ( $row == 1 ) {
+                                        if ( $request->getPut('name') != $result['name'] || $request->getPut('stock') != $result['stock'] ) {
+                                            if ( $request->getPut('name') != $result['name'] ) {
+                                                $raw_material->setName($request->getPut('name'));
+                                            } else {
+                                                $raw_material->setName($result['name']);
+                                            }
+        
+                                            if ( $request->getPut('stock') != $result['stock'] ) {
+                                                $raw_material->setStock(intval($request->getPut('stock')));
+                                            } else {
+                                                $raw_material->setStock($result['stock']);
+                                            }
+
+                                            $sql = '
+                                                UPDATE
+                                                    raw_materials
+                                                SET
+                                                    name = :name, stock = :stock
+                                                WHERE
+                                                    id = :id
+                                            ';
+        
+                                            try {
+                                                $this->db->begin();
+        
+                                                $update = $this->db->execute(
+                                                    $sql,
+                                                    [
+                                                        'id'    => $raw_material->getId(),
+                                                        'name'  => $raw_material->getName(),
+                                                        'stock' => $raw_material->getStock()
+                                                    ]
+                                                );
+        
+                                                if ( $update ) {
+                                                    $contents = [
+                                                        'msg' => 'Matéria-prima alterada com sucesso!'
+                                                    ];
+                            
+                                                    $response
+                                                        ->setJsonContent($contents, JSON_PRETTY_PRINT, 200)
+                                                        ->send();
+                                                } else {
+                                                    $contents = [
+                                                        'msg' => 'Matéria-prima não foi alterada, pois os campos estão com valores iguais!'
+                                                    ];
+                            
+                                                    $response
+                                                        ->setJsonContent($contents, JSON_PRETTY_PRINT, 400)
+                                                        ->send();
+                                                }
+        
+                                                $this->db->commit();
+                                            } catch (Exception $error) {
+                                                $this->db->rollback();
+                        
+                                                $contents = [
+                                                    'msg' => 'Ocorreu um erro em nosso servidor, tente mais tarde!'
+                                                ];
+                                
+                                                $response
+                                                    ->setJsonContent($contents, JSON_PRETTY_PRINT, 500)
+                                                    ->send();
+                                            }
+                                        } else {
+                                            $contents = [
+                                                'msg' => 'Digite pelo menos um campo com valor diferente do atual!'
+                                            ];
+                            
+                                            $response
+                                                ->setJsonContent($contents, JSON_PRETTY_PRINT, 400)
+                                                ->send();
+                                        }
+                                    } else {
+                                        $contents = [
+                                            'msg' => 'Matéria-prima não encontrada!'
+                                        ];
+                        
+                                        $response
+                                            ->setJsonContent($contents, JSON_PRETTY_PRINT, 400)
+                                            ->send();
+                                    }
+                                } else {
+                                    $contents = [
+                                        'msg' => 'Preencha um nome para o produto!'
+                                    ];
+                    
+                                    $response
+                                        ->setJsonContent($contents, JSON_PRETTY_PRINT, 400)
+                                        ->send();
+                                }
+                            } else {
+                                $contents = [
+                                    'msg' => 'Você não possui autorização para fazer alterações nesse usuário!'
+                                ];
+                
+                                $response
+                                    ->setJsonContent($contents, JSON_PRETTY_PRINT, 400)
+                                    ->send();
+                            }
+                        } else {
+                            $contents = [
+                                'msg' => 'Seu usuário não está ativo, contate um administrador ou RH!'
+                            ];
+            
+                            $response
+                                ->setJsonContent($contents, JSON_PRETTY_PRINT, 401)
+                                ->send();
+                        }
+                    } else {
+                        $contents = [
+                            'msg' => 'Não é possível acessar essa página, faça login!'
+                        ];
+        
+                        $response
+                            ->setJsonContent($contents, JSON_PRETTY_PRINT, 401)
+                            ->send();
+                    }
+                } else {
+                    $contents = [
+                        'msg' => 'Token inválido!'
+                    ];
+    
+                    $response
+                        ->setJsonContent($contents, JSON_PRETTY_PRINT, 401)
+                        ->send();
+                }
+            } else {
+                $contents = [
+                    'msg' => 'Seu usuário não está logado. Por favor, faça login!'
+                ];
+
+                $response
+                    ->setJsonContent($contents, JSON_PRETTY_PRINT, 401)
+                    ->send();
+            }
         }
 
         public function delete($id)
         {
+            $raw_material = new RawMaterial();
+            
+            $request = new Request();
 
+            $response = new Response();
+            
+            session_start();
+
+            if ( !empty($_SESSION['user']) && $this->redis->exists($_SESSION['user']) ) {
+                $bearerToken = $request->getHeaders()['Authorization'];
+
+                $bearerToken = str_replace('Bearer ', '', $bearerToken);
+
+                if ( !empty($bearerToken) && $bearerToken == $this->redis->get($_SESSION['user']) ) {
+                    $key = base64_encode($_ENV['SECRET_KEY'] . $_SESSION['user']);
+
+                    JWT::$leeway = 60;
+                    $token = JWT::decode($bearerToken, $key, array('HS512'));
+
+                    $token_array = (array) $token;
+                    $nbf_array = (array) $token_array['nbf'];
+
+                    if ( date(\DateTime::ISO8601) <= $nbf_array ) {
+                        if ( intval($token_array['situation']) == 1 ) {
+                            if ( intval($token_array['level']) == 0 || intval($token_array['level']) == 1 ) {
+                                $sql_verify_raw_material = '
+                                    SELECT
+                                        *
+                                    FROM
+                                        raw_materials
+                                    WHERE
+                                        id = :id
+                                ';
+
+                                $query = $this->db->query(
+                                    $sql_verify_raw_material,
+                                    [
+                                        'id' => $id
+                                    ]
+                                );
+
+                                $verify_raw_material_exists = $query->numRows();
+
+                                if ( $verify_raw_material_exists == 1 ) {
+                                    $raw_material->setId($id);
+
+                                    $sql = '
+                                        DELETE FROM
+                                            raw_materials
+                                        WHERE
+                                            id = :id
+                                    ';
+
+                                    try {
+                                        $this->db->begin();
+
+                                        $del = $this->db->execute(
+                                            $sql,
+                                            [
+                                                'id' => $raw_material->getId()
+                                            ]
+                                        );
+
+                                        if ( $del ) {
+                                            $contents = [
+                                                'msg' => 'Matéria-prima deletada com sucesso!'
+                                            ];
+                    
+                                            $response
+                                                ->setJsonContent($contents, JSON_PRETTY_PRINT, 200)
+                                                ->send();
+                                        } else {
+                                            $contents = [
+                                                'msg' => 'Não foi possível deletar matéria-prima'
+                                            ];
+                    
+                                            $response
+                                                ->setJsonContent($contents, JSON_PRETTY_PRINT, 400)
+                                                ->send();
+                                        }
+
+                                        $this->db->commit();
+                                    } catch (Exception $error) {
+                                        $this->db->rollback();
+                
+                                        $contents = [
+                                            'msg' => 'Ocorreu um erro em nosso servidor, tente mais tarde!'
+                                        ];
+                        
+                                        $response
+                                            ->setJsonContent($contents, JSON_PRETTY_PRINT, 500)
+                                            ->send();
+                                    }
+                                } else {
+                                    $contents = [
+                                        'msg' => 'Matéria-prima não encontrada!'
+                                    ];
+                    
+                                    $response
+                                        ->setJsonContent($contents, JSON_PRETTY_PRINT, 400)
+                                        ->send();
+                                }
+                            } else {
+                                $contents = [
+                                    'msg' => 'Você não possui autorização para fazer alterações nesse usuário!'
+                                ];
+                
+                                $response
+                                    ->setJsonContent($contents, JSON_PRETTY_PRINT, 400)
+                                    ->send();
+                            }
+                        } else {
+                            $contents = [
+                                'msg' => 'Seu usuário não está ativo, contate um administrador ou RH!'
+                            ];
+            
+                            $response
+                                ->setJsonContent($contents, JSON_PRETTY_PRINT, 401)
+                                ->send();
+                        }
+                    } else {
+                        $contents = [
+                            'msg' => 'Não é possível acessar essa página, faça login!'
+                        ];
+        
+                        $response
+                            ->setJsonContent($contents, JSON_PRETTY_PRINT, 401)
+                            ->send();
+                    }
+                } else {
+                    $contents = [
+                        'msg' => 'Token inválido!'
+                    ];
+    
+                    $response
+                        ->setJsonContent($contents, JSON_PRETTY_PRINT, 401)
+                        ->send();
+                }
+            } else {
+                $contents = [
+                    'msg' => 'Seu usuário não está logado. Por favor, faça login!'
+                ];
+
+                $response
+                    ->setJsonContent($contents, JSON_PRETTY_PRINT, 401)
+                    ->send();
+            }
         }
     }
 
