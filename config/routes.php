3e0ef636b02ec51aa7d16945fca2fb4e827e2001
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-14 15:35:45 -0300

    Adicionado rota de perfil e busca para usuários e novas rotas para cargo e status de pedidos. Também realizado correção no nome de rotas ou classes

--- config/routes.php
+++ config/routes.php
@@ -1,17 +1,32 @@
 <?php
 
     use Phalcon\Mvc\Micro\Collection as MicroCollection;
-    use App\Controllers\UserController;
-    use App\Controllers\ProductController;
+    use App\Controllers\RolesController;
+    use App\Controllers\UsersController;
+    use App\Controllers\ProductsController;
     use App\Controllers\CategoryController;
-    use App\Controllers\RawMaterialController;
+    use App\Controllers\RawMaterialsController;
+    use App\Controllers\StatusOrdersController;
     use App\Controllers\OrdersController;
     use App\Controllers\ProductionController;
 
+    $roles = new MicroCollection();
+    $roles->setHandler(new RolesController())
+          ->setPrefix('/api/roles')
+          ->get('/', 'index')
+          ->post('/register', 'register')
+          ->put('/update/{id}', 'update')
+          ->delete('/delete/{id}', 'delete');
+
+    $app->mount($roles);
+
     $users = new MicroCollection();
-    $users->setHandler(new UserController())
+    $users->setHandler(new UsersController())
           ->setPrefix('/api/users')
           ->get('/', 'index')
+          ->get('/profile', 'profile')
+          ->get('/profile/{id}', 'profile')
+          ->post('/search', 'search')
           ->post('/register', 'register')
           ->put('/update', 'update')
           ->put('/update/{id}', 'update')
@@ -25,7 +40,7 @@
     $app->mount($users);
 
     $products = new MicroCollection();
-    $products->setHandler(new ProductController())
+    $products->setHandler(new ProductsController())
              ->setPrefix('/api/products')
              ->get('/', 'index')
              ->post('/register', 'register')
@@ -45,7 +60,7 @@
     $app->mount($category);
 
     $rawMaterials = new MicroCollection();
-    $rawMaterials->setHandler(new RawMaterialController())
+    $rawMaterials->setHandler(new RawMaterialsController())
                  ->setPrefix('/api/raw-materials')
                  ->get('/', 'index')
                  ->post('/register', 'register')
@@ -53,6 +68,16 @@
                  ->delete('/delete/{id}', 'delete');
 
     $app->mount($rawMaterials);
+
+    $status_orders = new MicroCollection();
+    $status_orders->setHandler(new StatusOrdersController())
+           ->setPrefix('/api/status-orders')
+           ->get('/', 'index')
+           ->post('/register', 'register')
+           ->put('/update/{id}', 'update')
+           ->delete('/delete/{id}', 'delete');
+
+    $app->mount($status_orders);
 
     $orders = new MicroCollection();
     $orders->setHandler(new OrdersController())
