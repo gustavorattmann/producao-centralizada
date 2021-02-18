f43656acee9b90e4d7000bc7465773af695e497a
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-18 02:51:49 -0300

    Adicionado permissão de acesso CORS

--- public/index.php
+++ public/index.php
@@ -18,6 +18,10 @@
     require_once CONFIG_PATH . 'config.php';
     require_once CONFIG_PATH . 'di.php';
     require_once CONFIG_PATH . 'routes.php';
+
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
     
     $app->handle(
         $_SERVER["REQUEST_URI"]
